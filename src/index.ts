272087d11056356717420021f4fa5665262fe48a
aboveyou00 <brandonyoyoslade@gmail.com>
2017-03-23 20:55:07 -0600

    feat(exports): add SearchResults and CrudService

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,5 @@
-import { NgModule } from '@angular/core';
 
-//Imports
-import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
-export const module_exports = [];
-const imported_modules = [RouterModule, CommonModule, FormsModule, HttpModule];
-export const exported_modules = [...imported_modules];
-
-@NgModule({
-    declarations: [...module_exports],
-    imports: [...imported_modules],
-    exports: [...module_exports, ...exported_modules]
-})
-export class MiterSharedModule {
-}
+export * from './module';
+export * from './services';
+export * from './util';
