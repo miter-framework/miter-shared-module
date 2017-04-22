58fa90c4a8525eda5e5bf6dae77edc3ea4a4db9d
aboveyou00 <brandonyoyoslade@gmail.com>
2017-04-22 15:10:58 -0600

    feat(crud-service): add findOne; refactor CrudService; add CHANGELOG.md

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+
+<a name="0.2.0"></a>
+## [0.2.0](https://github.com/miter-framework/miter/compare/0.1.5...HEAD) (_Future_)
+
+### Features
+
+* **crud-service:** add findOne helper method
+* **crud-service:** refactor duplicated code into extensible methods
+
+
+
+<a name="0.1.5"></a>
+## [0.1.5](https://github.com/miter-framework/miter/tree/0.1.5) (2017-04-13)
+
+Changes before this point in time have not been recorded here.
--- src/services/crud-service.ts
+++ src/services/crud-service.ts
@@ -1,11 +1,10 @@
-import { Injectable } from '@angular/core';
-import { Http, URLSearchParams } from '@angular/http';
+import { Http, URLSearchParams, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/share';
 import 'rxjs/add/operator/catch';
-import cloneDeep = require('lodash.clonedeep');
+import merge = require('lodash.merge');
 
 import { DEFAULT_PER_PAGE, SearchResults } from '../util/search-results';
 import { HTTP_STATUS_OK } from '../util/http-status-type';
@@ -13,7 +12,6 @@ import { pluralize } from '../util/pluralize';
 
 export type FromJsonFn<T> = { (json: any): T | null };
 
-@Injectable()
 export abstract class CrudService<T> {
     constructor(private _http: Http, private modelName: string, private fromJson: FromJsonFn<T>, protected readonly singularPath?: string, protected readonly pluralPath?: string) {
         let parts = this.splitOnWords(modelName).filter(Boolean);
@@ -28,120 +26,10 @@ export abstract class CrudService<T> {
         }
     }
     
-    protected transformRoute(url: string, otherParams: { [key: string]: any }): string {
-        let keys = Object.keys(otherParams);
-        for (let q = 0; q < keys.length; q++) {
-            let key = keys[q];
-            const regex = new RegExp(`:${key}`)
-            let matches = url.match(regex);
-            if (matches != null && matches.length) {
-                let val = otherParams[key];
-                if (val.id) val = val.id;
-                delete otherParams[key];
-                url = url.replace(regex, `${val}`);
-            }
-        }
-        return url;
-    }
-    
     abstract get basePath(): string;
     
     protected get http() {
         return this._http;
-    }
-    
-    create(data: any, otherParams: { [key: string]: any } = {}): Observable<T> {
-        let url = this.transformRoute(`${this.basePath}${this.pluralPath}/create`, otherParams);
-        let params = new URLSearchParams();
-        for (let key in otherParams) {
-            let value = otherParams[key];
-            params.set(key, typeof value === 'string' ? value : JSON.stringify(value));
-        }
-        return this.http.post(url, data, { search: params })
-          .map(response => {
-              let result = this.fromJson(response.json());
-              if (!result) throw new Error(`Failed to deserialize ${this.modelName} after creating it.`);
-              return result;
-          })
-          .share();
-    }
-    
-    find(query: Object, page: number = 0, perPage: number = DEFAULT_PER_PAGE, otherParams: { [key: string]: any } = {}): Observable<SearchResults<T>> {
-        let url = this.transformRoute(`${this.basePath}${this.pluralPath}/find`, otherParams);
-        let params = new URLSearchParams();
-        query = cloneDeep(query);
-        delete (<any>query)['requestingUser'];
-        params.set('query', JSON.stringify(query));
-        params.set('page', `${page}`);
-        params.set('perPage', `${perPage}`);
-        for (let key in otherParams) {
-            let value = otherParams[key];
-            params.set(key, typeof value === 'string' ? value : JSON.stringify(value));
-        }
-        return this.http.get(url, { search: params })
-          .map(response => SearchResults.fromJson(response.json(), this.fromJson, query))
-          .share();
-    }
-    
-    get(id: number, otherParams: { [key: string]: any } = {}): Observable<T | null> {
-        let url = this.transformRoute(`${this.basePath}${this.singularPath}/${id}`, otherParams);
-        let params = new URLSearchParams();
-        for (let key in otherParams) {
-            let value = otherParams[key];
-            params.set(key, typeof value === 'string' ? value : JSON.stringify(value));
-        }
-        return this.http.get(url, { search: params })
-          .map(response => this.fromJson(response.json()))
-          .share();
-    }
-
-    count(query: Object, otherParams: { [key: string]: any } = {}): Observable<number> {
-        let url = this.transformRoute(`${this.basePath}${this.pluralPath}/count`, otherParams);
-        let params = new URLSearchParams();
-        query = cloneDeep(query);
-        delete (<any>query)['requestingUser'];
-        params.set('query', JSON.stringify(query));
-        for (let key in otherParams) {
-            let value = otherParams[key];
-            params.set(key, typeof value === 'string' ? value : JSON.stringify(value));
-        }
-        return this.http.get(url, { search: params })
-          .map(response => parseInt(response.text()))
-          .share();
-    }
-    
-    update(id: number, data: any, returning: boolean = false, otherParams: { [key: string]: any } = {}): Observable<T> {
-        let url = this.transformRoute(`${this.basePath}${this.singularPath}/${id}`, otherParams);
-        let params = new URLSearchParams();
-        params.set('returning', `${!!returning}`);
-        for (let key in otherParams) {
-            let value = otherParams[key];
-            params.set(key, typeof value === 'string' ? value : JSON.stringify(value));
-        }
-        return this.http.put(url, data, { search: params })
-          .map(response => {
-              if (returning) {
-                  let result = this.fromJson(response.json());
-                  if (!result) throw new Error(`Failed to deserialize ${this.modelName} after updating it`);
-                  return result;
-              }
-              else
-                  return null;
-          })
-          .share();
-    }
-    
-    destroy(id: number, otherParams: { [key: string]: any } = {}): Observable<boolean> {
-        let url = this.transformRoute(`${this.basePath}${this.singularPath}/${id}`, otherParams);
-        let params = new URLSearchParams();
-        for (let key in otherParams) {
-            let value = otherParams[key];
-            params.set(key, typeof value === 'string' ? value : JSON.stringify(value));
-        }
-        return this.http.delete(url, { search: params })
-          .map(response => response.status === HTTP_STATUS_OK)
-          .catch(err => Observable.of(false))
-          .share();
     }
     
     private splitOnWords(name: string): string[] {
@@ -161,5 +49,114 @@ export abstract class CrudService<T> {
         }
         if (currentWord) results.push(currentWord);
         return results;
+    }
+    
+    create(data: any, otherParams: { [key: string]: any } = {}): Observable<T> {
+        let params = merge({}, otherParams);
+        return this.performPost(`${this.basePath}${this.pluralPath}/create`, data, params)
+          .map(response => {
+              let result = this.fromJson(response.json());
+              if (!result) throw new Error(`Failed to deserialize ${this.modelName} after creating it.`);
+              return result;
+          })
+          .share();
+    }
+    
+    find(query: Object, page: number = 0, perPage: number = DEFAULT_PER_PAGE, otherParams: { [key: string]: any } = {}): Observable<SearchResults<T>> {
+        let params = merge({}, otherParams, { query: this.transformQuery(query), page: page, perPage: perPage });
+        return this.performGet(`${this.basePath}${this.pluralPath}/find`, params)
+          .map(response => SearchResults.fromJson(response.json(), this.fromJson, query))
+          .share();
+    }
+    findOne(query: Object, otherParams: { [key: string]: any } = {}): Observable<T | null> {
+        let params = merge({}, otherParams, { query: this.transformQuery(query) });
+        return this.performGet(`${this.basePath}${this.pluralPath}/find-one`, params)
+          .map(response => this.fromJson(response.json()))
+          .share();
+    }
+    get(id: number, otherParams: { [key: string]: any } = {}): Observable<T | null> {
+        let params = merge({}, otherParams, { id: id });
+        return this.performGet(`${this.basePath}${this.singularPath}/:id`, params)
+          .map(response => this.fromJson(response.json()))
+          .share();
+    }
+    count(query: Object, otherParams: { [key: string]: any } = {}): Observable<number> {
+        let params = merge({}, otherParams, { query: this.transformQuery(query) });
+        return this.performGet(`${this.basePath}${this.pluralPath}/count`, params)
+          .map(response => parseInt(response.text()))
+          .share();
+    }
+    
+    update(id: number, data: any, returning: boolean = false, otherParams: { [key: string]: any } = {}): Observable<T> {
+        let params = merge({}, otherParams, { id: id, returning: !!returning });
+        return this.performPut(`${this.basePath}${this.pluralPath}/:id`, data, params)
+          .map(response => {
+              if (!returning) return null;
+              let result = this.fromJson(response.json());
+              if (!result) throw new Error(`Failed to deserialize ${this.modelName} after updating it`);
+              return result;
+          })
+          .share();
+    }
+    
+    destroy(id: number, otherParams: { [key: string]: any } = {}): Observable<boolean> {
+        let params = merge({}, otherParams, { id: id });
+        return this.performDelete(`${this.basePath}${this.singularPath}/:id`, params)
+          .map(response => response.status === HTTP_STATUS_OK)
+          .catch(err => Observable.of(false))
+          .share();
+    }
+    
+    protected performPost(origUrl: string, data: any, origParams: { [key: string]: any } = {}): Observable<Response> {
+        let [url, params] = this.transformParams(origUrl, origParams);
+        return this.http.post(url, data, { search: params });
+    }
+    protected performPut(origUrl: string, data: any, origParams: { [key: string]: any } = {}): Observable<Response> {
+        let [url, params] = this.transformParams(origUrl, origParams);
+        return this.http.put(url, data, { search: params });
+    }
+    protected performGet(origUrl: string, origParams: { [key: string]: any } = {}): Observable<Response> {
+        let [url, params] = this.transformParams(origUrl, origParams);
+        return this.http.get(url, { search: params });
+    }
+    protected performDelete(origUrl: string, origParams: { [key: string]: any } = {}): Observable<Response> {
+        let [url, params] = this.transformParams(origUrl, origParams);
+        return this.http.delete(url, { search: params });
+    }
+    
+    private transformParams(origUrl: string, origParams: { [key: string]: any } = {}): [string, Object] {
+        let url = this.transformRoute(origUrl, origParams);
+        let params = new URLSearchParams();
+        for (let key in origParams) {
+            let value = origParams[key];
+            let serializedValue = this.serializeValue(value);
+            if (value === null || typeof value === 'undefined') continue;
+            params.set(key, serializedValue);
+        }
+        return [url, params];
+    }
+    
+    protected transformRoute(url: string, otherParams: { [key: string]: any }): string {
+        let keys = Object.keys(otherParams);
+        for (let q = 0; q < keys.length; q++) {
+            let key = keys[q];
+            const regex = new RegExp(`:${key}`)
+            let matches = url.match(regex);
+            if (matches != null && matches.length) {
+                let val = otherParams[key];
+                delete otherParams[key];
+                url = url.replace(regex, `${this.serializeValue(val)}`);
+            }
+        }
+        return url;
+    }
+    protected serializeValue(value: any) {
+        return !value || typeof value === 'string' ? value :
+          value.id && typeof value.id === 'number' ? value.id :
+                                                     JSON.stringify(value);
+    }
+    
+    protected transformQuery(query: Object): string {
+        return JSON.stringify(query);
     }
 }
